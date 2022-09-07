5f183d46999ed51df96ca00f6c8eed3c95c5aabc
joruy12 <jyymi1011@gmail.com>
2022-09-07 17:26:38 +0300

    done5

--- SHAML2/app/src/main/java/com/example/shaml/MainActivity.java
+++ SHAML2/app/src/main/java/com/example/shaml/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.shaml;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,6 +20,8 @@ import android.widget.Toast;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -32,6 +41,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
           quiz4= findViewById(R.id.quiz);
          Calculator5 = findViewById(R.id.Calculator);
           btnCall= findViewById(R.id.callbtn);
+
+          MaterialToolbar toolbar = findViewById(R.id.topAppbar);
+          DrawerLayout drawerLayout = findViewById(R.id.drawer_layoyt);
+          NavigationView navigationView = findViewById(R.id.navigation_view);
 
         imageSlider = findViewById(R.id.image_slider);
 
@@ -62,6 +75,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
                  Toast.makeText(MainActivity.this, "سوف يتم تحويلك للتواصل معنا", Toast.LENGTH_LONG).show();
+             }
+         });
+
+         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+
+                 drawerLayout.openDrawer(GravityCompat.START);
+
+             }
+         });
+
+         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+             @Override
+             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                 int id = item.getItemId();
+                 item.setChecked(true);
+                 drawerLayout.closeDrawer(GravityCompat.START);
+                 switch (id){
+                     case R.id.nav_aboutshaml:
+                      startActivity(new Intent(MainActivity.this,AboutActivity.class));
+                      break;
+                     case R.id.nav_add:
+                         startActivity(new Intent(MainActivity.this,AddlistActivity.class));
+                    break;
+                     case R.id.nav_share:
+                         Toast.makeText(MainActivity.this,"شكرا لمشاركة تطبيقنا",Toast.LENGTH_SHORT).show();break;
+                     case R.id.nav_rate:
+                         Toast.makeText(MainActivity.this,"شكرا للتقييم",Toast.LENGTH_SHORT).show();break;
+                     default:
+                         return true;
+
+
+                 }
+
+                return true;
              }
          });
     }
@@ -99,4 +149,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+
+
 }
